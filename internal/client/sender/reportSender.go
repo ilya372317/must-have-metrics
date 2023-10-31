420fcc6e537869b4c424674541e2ceacf888b486
Ilya Otinov <ilya.otinov@gmail.com>
2023-10-31 11:53:38 +0700

    fix: add body close for response

--- internal/client/sender/reportSender.go
+++ internal/client/sender/reportSender.go
@@ -9,9 +9,9 @@ type ReportSender func(requestURL string)
 
 func SendReport(requestURL string) {
 	res, err := http.Post(requestURL, "text/plain", nil)
-	defer res.Body.Close()
+
 	if err != nil {
 		log.Printf("failed to save data on server: %v\n", err)
 	}
-
+	defer res.Body.Close()
 }
