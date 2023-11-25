7e3c9cd602090a2878186d5641949cdf3ba909b5
Ilya Otinov <ilya.otinov@gmail.com>
2023-11-25 14:21:11 +0700

    fix: add some constants

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,7 +13,12 @@ import (
 	"github.com/ilya372317/must-have-metrics/internal/utils/logger"
 )
 
-const storePathAlias = "store_path"
+const (
+	storePathAlias     = "store_path"
+	hostAlias          = "host"
+	restoreAlias       = "restore"
+	storeIntervalAlias = "store_interval"
+)
 
 var (
 	repository *storage.InMemoryStorage
@@ -27,7 +32,7 @@ func main() {
 	repository = storage.NewInMemoryStorage()
 
 	cnfg := config.GetServerConfig()
-	isRestart, err := strconv.ParseBool(cnfg.GetValue("restore"))
+	isRestart, err := strconv.ParseBool(cnfg.GetValue(restoreAlias))
 	if err != nil {
 		sLogger.Panicf("invalid restart configuration value: %v", err)
 	}
@@ -37,7 +42,7 @@ func main() {
 		}
 	}
 
-	storeInterval, err := strconv.Atoi(cnfg.GetValue("store_interval"))
+	storeInterval, err := strconv.Atoi(cnfg.GetValue(storeIntervalAlias))
 	if err != nil {
 		sLogger.Panicf("failed parse store interval parameter")
 	}
@@ -51,13 +56,13 @@ func main() {
 	}
 
 	if err = run(cnfg, storeInterval == 0, cnfg.GetValue(storePathAlias)); err != nil {
-		sLogger.Panicf("failed to start server on address: %s, %v", cnfg.GetValue("host"), err)
+		sLogger.Panicf("failed to start server on address: %s, %v", cnfg.GetValue(hostAlias), err)
 	}
 }
 
 func run(cnfg *config.ServerConfig, isSyncSaving bool, fileStoragePath string) error {
 	sLogger.Infof("server is starting...")
-	err := http.ListenAndServe(cnfg.GetValue("host"), router.AlertRouter(repository, isSyncSaving, fileStoragePath))
+	err := http.ListenAndServe(cnfg.GetValue(hostAlias), router.AlertRouter(repository, isSyncSaving, fileStoragePath))
 	if err != nil {
 		return fmt.Errorf("failed to start server: %w", err)
 	}
