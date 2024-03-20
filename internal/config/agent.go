031bdd0ecb6af2a12bfce46a01edbc0305750e59
Ilya Otinov <ilya.otinov@gmail.com>
2024-03-20 19:40:46 +0700

    feat: finish config for agent

--- internal/config/agent.go
+++ internal/config/agent.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -8,14 +9,36 @@ import (
 	"github.com/caarlos0/env"
 )
 
+const (
+	defaultRateLimitValue      = 1
+	defaultPollIntervalValue   = 2
+	defaultReportIntervalValue = 10
+	defaultAddressValue        = "localhost:8080"
+	defaultSecretKeyValue      = ""
+	defaultCryptoKeyValue      = ""
+	defaultConfigValue         = ""
+
+	nullStringValue = ""
+	nullIntValue    = 0
+)
+
 // AgentConfig agent configs.
+//
+// Adding new filed steps:
+// 1. Add new field to this struct.
+// 2. Fill env and json struct tags.
+// 3. Add parsing new field in parseFlags method.
+// 4. Add parsing new field in parseFromFileMethod.
+//
+// Note: for default config values use constants.
 type AgentConfig struct {
-	Host           string `env:"ADDRESS"`
-	SecretKey      string `env:"KEY"`
-	CryptoKey      string `env:"CRYPTO_KEY"`
-	PollInterval   uint   `env:"POLL_INTERVAL"`
-	ReportInterval uint   `env:"REPORT_INTERVAL"`
-	RateLimit      uint   `env:"RATE_LIMIT"`
+	Host           string `env:"ADDRESS" json:"address,omitempty"`
+	SecretKey      string `env:"KEY" json:"secret_key,omitempty"`
+	CryptoKey      string `env:"CRYPTO_KEY" json:"crypto_key,omitempty"`
+	PollInterval   uint   `env:"POLL_INTERVAL" json:"poll_interval,omitempty"`
+	ReportInterval uint   `env:"REPORT_INTERVAL" json:"report_interval,omitempty"`
+	RateLimit      uint   `env:"RATE_LIMIT" json:"rate_limit,omitempty"`
+	ConfigPath     string `env:"CONFIG"`
 }
 
 // NewAgent constructor for AgentConfig.
@@ -25,6 +48,9 @@ func NewAgent() (*AgentConfig, error) {
 	if err := env.Parse(agentConfig); err != nil {
 		return nil, fmt.Errorf("failed parse agent flags: %w", err)
 	}
+	if err := agentConfig.parseFromFile(); err != nil {
+		return nil, fmt.Errorf("failed parse agent config from file: %w", err)
+	}
 
 	return agentConfig, nil
 }
@@ -32,14 +58,53 @@ func NewAgent() (*AgentConfig, error) {
 func (c *AgentConfig) parseFlags() {
 	flag.StringVar(
 		&c.Host, "a",
-		"localhost:8080", "address where server will listen requests",
+		defaultAddressValue, "address where server will listen requests",
 	)
-	flag.UintVar(&c.PollInterval, "p", 2, "interval agent collect metrics")
-	flag.UintVar(&c.ReportInterval, "r", 10, "interval agent send metrics on server")
-	flag.StringVar(&c.SecretKey, "k", "", "secret key for sign")
-	flag.UintVar(&c.RateLimit, "l", 1, "limit of simultaneously requests to server")
-	flag.StringVar(&c.CryptoKey, "crypto-key", "", "public crypto key for cipher transferred data")
+	flag.UintVar(&c.PollInterval, "p", defaultPollIntervalValue, "interval agent collect metrics")
+	flag.UintVar(&c.ReportInterval, "r", defaultReportIntervalValue, "interval agent send metrics on server")
+	flag.StringVar(&c.SecretKey, "k", defaultSecretKeyValue, "secret key for sign")
+	flag.UintVar(&c.RateLimit, "l", defaultRateLimitValue, "limit of simultaneously requests to server")
+	flag.StringVar(&c.CryptoKey, "crypto-key", defaultCryptoKeyValue, "public crypto key for cipher transferred data")
+	flag.StringVar(&c.ConfigPath, "c", defaultConfigValue, "file path to json configuration file")
 	flag.Parse()
+}
+
+// parseFromFile fill config fields from given file. File required be in json format.
+func (c *AgentConfig) parseFromFile() error {
+	if c.ConfigPath == "" {
+		return nil
+	}
+	fileContent, err := getConfigFileContent(c.ConfigPath)
+	if err != nil {
+		return fmt.Errorf("failed parse agent config from file: %w", err)
+	}
+
+	tempConfig := AgentConfig{}
+
+	if err = json.Unmarshal(fileContent, &tempConfig); err != nil {
+		return fmt.Errorf("invalid config file content: %w", err)
+	}
+
+	if c.Host == defaultAddressValue || c.Host == nullStringValue {
+		c.Host = tempConfig.Host
+	}
+	if c.SecretKey == defaultSecretKeyValue {
+		c.SecretKey = tempConfig.SecretKey
+	}
+	if c.CryptoKey == defaultCryptoKeyValue {
+		c.CryptoKey = tempConfig.CryptoKey
+	}
+	if c.PollInterval == defaultPollIntervalValue || c.PollInterval == nullIntValue {
+		c.PollInterval = tempConfig.PollInterval
+	}
+	if c.ReportInterval == defaultReportIntervalValue || c.ReportInterval == nullIntValue {
+		c.ReportInterval = tempConfig.ReportInterval
+	}
+	if c.RateLimit == defaultRateLimitValue || c.RateLimit == nullIntValue {
+		c.RateLimit = tempConfig.RateLimit
+	}
+
+	return nil
 }
 
 // ShouldSignData check if agent configured for sign sending data.
@@ -47,6 +112,7 @@ func (c *AgentConfig) ShouldSignData() bool {
 	return c.SecretKey != ""
 }
 
+// ShouldCipherData check if agent configured for crypt sending data.
 func (c *AgentConfig) ShouldCipherData() bool {
 	if c.CryptoKey == "" {
 		return false
---
+++ internal/config/agent_test.go
@@ -0,0 +1,114 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAgentConfig_parseFromFile(t *testing.T) {
+	const tempFileConfigPath = "/tmp/agent-config.json"
+	tests := []struct {
+		name        string
+		baseConfig  AgentConfig
+		fileConfigs AgentConfig
+		filePath    string
+		wantErr     bool
+		want        AgentConfig
+	}{
+		{
+			name:        "invalid file path case",
+			baseConfig:  AgentConfig{},
+			fileConfigs: AgentConfig{},
+			filePath:    "/tmp/invalid-file-path.json",
+			wantErr:     true,
+			want:        AgentConfig{},
+		},
+		{
+			name: "success case with default base config",
+			baseConfig: AgentConfig{
+				Host:           defaultAddressValue,
+				SecretKey:      defaultSecretKeyValue,
+				CryptoKey:      defaultCryptoKeyValue,
+				PollInterval:   defaultPollIntervalValue,
+				ReportInterval: defaultReportIntervalValue,
+				RateLimit:      defaultRateLimitValue,
+				ConfigPath:     defaultConfigValue,
+			},
+			fileConfigs: AgentConfig{
+				Host:           "localhost:9090",
+				SecretKey:      "123",
+				CryptoKey:      "321",
+				PollInterval:   5,
+				ReportInterval: 15,
+				RateLimit:      20,
+			},
+			filePath: tempFileConfigPath,
+			wantErr:  false,
+			want: AgentConfig{
+				Host:           "localhost:9090",
+				SecretKey:      "123",
+				CryptoKey:      "321",
+				PollInterval:   5,
+				ReportInterval: 15,
+				RateLimit:      20,
+				ConfigPath:     tempFileConfigPath,
+			},
+		},
+		{
+			name: "no effect case",
+			baseConfig: AgentConfig{
+				Host:           "localhost:8090",
+				SecretKey:      "123",
+				CryptoKey:      "123",
+				PollInterval:   4,
+				ReportInterval: 5,
+				RateLimit:      6,
+			},
+			fileConfigs: AgentConfig{
+				Host:           "localhost:8091",
+				SecretKey:      "321",
+				CryptoKey:      "321",
+				PollInterval:   5,
+				ReportInterval: 6,
+				RateLimit:      7,
+			},
+			filePath: tempFileConfigPath,
+			wantErr:  false,
+			want: AgentConfig{
+				Host:           "localhost:8090",
+				SecretKey:      "123",
+				CryptoKey:      "123",
+				PollInterval:   4,
+				ReportInterval: 5,
+				RateLimit:      6,
+				ConfigPath:     tempFileConfigPath,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			configData, err := json.Marshal(&tt.fileConfigs)
+			require.NoError(t, err)
+			err = os.WriteFile(tempFileConfigPath, configData, 0650)
+			require.NoError(t, err)
+
+			tt.baseConfig.ConfigPath = tt.filePath
+			err = tt.baseConfig.parseFromFile()
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			} else {
+				require.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.want, tt.baseConfig)
+
+			err = os.RemoveAll(tempFileConfigPath)
+			require.NoError(t, err)
+		})
+	}
+}
